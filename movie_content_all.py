83a9bb7375084e9d0e1229032460328653e3f4f3
Eddie Chen <122704850+eddiemaximchen@users.noreply.github.com>
2023-02-11 22:59:19 +0800

    Add files via upload
    get all movie(Theater/home)

---
+++ movie_content_all.py
@@ -0,0 +1,78 @@
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+import json
+import pandas as pd
+def getMovieinfo(path):
+    try:
+        html = urlopen(path)
+        bs = BeautifulSoup(html.read(),'html.parser')
+        data=bs.find_all('h1')
+        movie=[]
+        content_str=''
+        title=data[0].get_text()
+        content_str=content_str+title
+
+        #year and movie type
+        data=bs.find('p',{'class':'scoreboard__info'}).get_text()
+        data1=data.split(',')
+        length=len(data1)
+        for i in range(length):
+            content_str=content_str+'*'+data1[i].strip()
+        if length==1:
+            content_str=content_str+'* '
+        #AUDIENCE SCORE
+        data=bs.find('score-board')
+        content_str=content_str+'*'+data.attrs['audiencescore']
+        if data.attrs['audiencescore']=="":
+            content_str=content_str+'*'
+        #storyline
+        story=bs.find('div',{'id':'movieSynopsis'}).get_text().strip('\n')
+        if story=='':
+            content_str=content_str+'*'+'N/A'
+        else:
+            content_str=content_str+'*'+story.strip()
+        # stars
+        stars=bs.find_all('div',{'class':'cast-item'})
+        content_str=content_str+'*'
+        for star in stars:
+            starName=star.get_text().strip()
+            if 'Unknown' in starName:
+                continue
+            else:
+                content_str=content_str+starName[:20].strip()+' '
+        movie=content_str.split('*')
+        movie1=[]
+        if length==1:
+            movie1.append({
+                'name':movie[0],
+                'year':movie[1],
+                'type':'',
+                'howlong':movie[2],
+                'score':movie[3],
+                'story':movie[4],
+                'stars':movie[5]
+                })
+        else:
+             movie1.append({
+                'name':movie[0],
+                'year':movie[1],
+                'type':movie[2],
+                'howlong':movie[3],
+                'score':movie[4],
+                'story':movie[5],
+                'stars':movie[6]
+            })
+             
+        
+        #save as json
+        with open(f"home/{title}.json", "w", encoding="utf-8") as file:
+            file.write(json.dumps(movie1, ensure_ascii=False, indent=4))
+
+    except Exception as err:
+        print(path)
+        print(str(err))
+        exit()
+#讀入電影路徑
+theater=pd.read_json('movies_athome_150.json')
+for movie in theater['path']:
+            getMovieinfo(movie)
